fd36935fa4ad10429e06717e7244a26ee0cb0ab1
Aid <kostyuchik22@gmail.com>
2023-10-24 19:58:09 +0300

    add solution for examination money box

---
+++ examination/money_box.py
@@ -0,0 +1,47 @@
+from collections import Counter
+
+
+def visualize(money, bar_char='₽'):
+    cnt = Counter(money)
+    result = ''
+    a = cnt.most_common(1)
+    index = a[0][1]
+    while index >= 0:
+        for num in sorted(cnt):
+            if cnt[num] == index:
+                result = result + str(cnt[num]) + '  '
+            elif cnt[num] > index:
+                result = result + bar_char * 2 + ' '
+            else:
+                result = result + '   '
+        index -= 1
+        result = result[:-1] + '\n'
+    for i in range(len(cnt)):
+        result = result + '---'
+    result = result[:-1] + '\n'
+    for elem in sorted(cnt):
+        if elem < 10:
+            result = result + str(elem) + '  '
+        else:
+            result = result + str(elem) + ' '
+    return result[:-1]
+
+
+MONEY = (
+    1, 20, 2, 5, 20,
+    3, 5, 2, 10, 2,
+    20, 2, 20, 1, 2,
+    1, 1, 2, 10, 20, 3,
+)
+
+assert visualize(MONEY, bar_char='$') == """
+   6             
+   $$          5 
+4  $$          $$
+$$ $$          $$
+$$ $$ 2  2  2  $$
+$$ $$ $$ $$ $$ $$
+$$ $$ $$ $$ $$ $$
+-----------------
+1  2  3  5  10 20
+"""[1:-1]
