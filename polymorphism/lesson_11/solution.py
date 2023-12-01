c5e7af9ccb2887df38e03de2f4948556671de45d
Aid <kostyuchik22@gmail.com>
2023-12-01 13:14:43 +0300

    add solution for lesson 11 (polymorphism)

--- polymorphism/lesson_11/solution.py
+++ polymorphism/lesson_11/solution.py
@@ -4,9 +4,12 @@ from gateway import find_all_matching
 def find_the_cheapest_service(data, predicates={}):
 # BEGIN (write your solution here)
     hotels_info = find_all_matching(data, predicates)
+    print(hotels_info)
     result = {}
     for elem in hotels_info:
-        if elem['hotels'] not in result:
+        if result == {}:
+            result.update(elem)
+        if elem['hotel']['cost'] < result['hotel']['cost']:
             result.update(elem)
     return result
 # END
