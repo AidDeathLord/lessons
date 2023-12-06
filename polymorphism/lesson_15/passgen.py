571a6ac99705c29fbe4a24e299cf290b8ac25ae6
Aid <kostyuchik22@gmail.com>
2023-12-06 17:05:43 +0300

    add solution for lesson 15 (polymorphism)

--- polymorphism/lesson_15/passgen.py
+++ polymorphism/lesson_15/passgen.py
@@ -11,11 +11,8 @@ def generate_password(len, uppercase=False, digits=False, symbols=False):
     gen_pool = ascii_lowercase
     if uppercase:
         gen_pool += ascii_uppercase
-        print(1)
     if digits:
         gen_pool += ascii_digits
-        print(2)
     if symbols:
         gen_pool += punctuation
-        print(3)
     return ''.join(random.choice(gen_pool) for _ in range(len))
