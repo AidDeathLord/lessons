857be1f56c2117acecabb7efcef8fd6809c9e808
Aid <kostyuchik22@gmail.com>
2023-12-11 14:50:10 +0300

    add solution for lesson 17 (polymorphism)

---
+++ polymorphism/lesson_17/mixins.py
@@ -0,0 +1,61 @@
+import json
+from classes import User as BaseUser, ShoppingCart as BaseCart
+
+
+class EqualityMixin:
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return self.__dict__ == other.__dict__
+
+
+class SerializeMixin:
+    def serialize(self):
+        return json.dumps(self.__dict__)
+
+
+
+    @classmethod
+    def deserialize(cls, data):
+        attrs = json.loads(data)
+        instance = cls(**attrs)
+        return instance
+
+
+
+
+
+class User(BaseUser, EqualityMixin):
+    pass
+
+
+user1 = User('John', 'john@mail.com', 'password')
+user2 = User('John', 'john@mail.com', 'password')
+user3 = User('Alice', 'alice@mail.com', 'password')
+
+assert user1 == user2
+assert user1 != user3
+
+
+class Cart(BaseCart, EqualityMixin):
+    pass
+
+
+cart1 = Cart(['cat', 'dog'])
+cart2 = Cart(['cat'])
+cart3 = Cart(['cat', 'cat', 'dog'])
+cart2.add_item('dog')
+assert cart1 == cart2
+assert cart1 != cart3
+user4 = BaseUser('John', 'john@mail.com', 'password')
+assert user1 != user4, 'Mixin should compare same class'
+
+
+class Cart(BaseCart, EqualityMixin, SerializeMixin):
+    pass
+
+
+cart1 = Cart(['cat', 'dog'])
+data1 = cart1.serialize()
+cart2 = Cart.deserialize(data1)
+assert cart1 == cart2, 'EqualityMixin should work'
+assert cart1 is not cart2, 'SerializeMixin should return new object'
